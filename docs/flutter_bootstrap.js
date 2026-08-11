2b8ba7006bdbb8866674b9856c3ee1e2fa7b0c38
Firebomb5516 <clevertortoise1@gmail.com>
2026-08-11 17:45:15 +1000

    WEB APP REDO

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "733983494" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3326089871" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
