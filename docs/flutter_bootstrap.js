61f7b2ca986e40a67867e7265d0fa30738071863
Firebomb5516 <clevertortoise1@gmail.com>
2026-08-12 21:39:05 +1000

    Update flutter_bootstrap.js

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "323943063" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2513060068" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
